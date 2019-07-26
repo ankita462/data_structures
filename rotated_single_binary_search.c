45d023f5a42892b103ddb1a6ebd50b7178990b80
Ankita Garg <ankitagarg462@gmail.com>
2019-07-26 12:57:52 -0400

    rotated single binary search

--- rotated_single_binary_search.c
+++ rotated_single_binary_search.c
@@ -20,17 +20,18 @@ int binary_single_search(int arr[],int low,int high,int element) {
 	if(low>high)
 		return -1;
 	int mid=low+(high-low)/2;
-	if(arr[low]<arr[mid] && element<arr[mid] && element>arr[low])
-	{
-		return binary_single_search(arr,mid-1,high,element);
-	}
-	else if(arr[mid+1]<arr[high] && element>arr[mid+1] && element<arr[high])
-	{
-		return binary_single_search(arr,mid+1,high,element);
-	}
-	else if(element==arr[mid])
-	{
+	if(arr[mid]==element)
 		return mid;
+	if(arr[low]<=arr[mid])
+	{
+		if(element>=arr[low] && element<=arr[mid])
+			return binary_single_search(arr,low,mid-1,element);
+		else
+			return binary_single_search(arr,mid+1,high,element);
 	}
+		if(element>=arr[mid+1] && element<=arr[high])
+			return binary_single_search(arr,mid+1,high,element);
+		else
+			return binary_single_search(arr,low,mid-1,element);
 }
 	  
