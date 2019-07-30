d08d8cb191b8b4e0a82d2320760fee8672ece4ed
Ankita Garg <ankitagarg462@gmail.com>
2019-07-30 15:11:44 -0400

    reverse string

--- reverse_string.cpp
+++ reverse_string.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+char* reverse_string(char *str,int len);
 
 int main(int argc,char *argv[]) {
 	char *str;
 	str=argv[1];
 	int len=strlen(str);
-	for(int i=len;i>=0;i--)
-	{
-		cout<<str[i];
-	}
+	char *str1=reverse_string(str,len);
+	cout<<str1;
 	return 0;
+}
+char* reverse_string(char *str,int len)
+{
+	char temp;
+	for(int i=0;i<len/2;i++)
+	{
+		temp=str[i];
+		str[i]=str[len-i-1];
+		str[len-1-i]=temp;
+	}
+	return str;
 }
 
