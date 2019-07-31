fd63ab89e43e5fe69ab18f85058ae085620023f9
Ankita Garg <ankitagarg462@gmail.com>
2019-08-01 00:40:29 +0530

    remove duplicate from string

---
+++ remove_duplicate.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void remove_duplicate(char *str,int len)
+{
+	for(int i=0;i<len;i++)
+	{
+		for(int j=i+1;j<len;j++)
+		{
+			if(str[i]==str[j])
+			{
+				str[j]='\0';
+			}
+		}
+	}
+	for(int i=0;i<len;i++)
+	{
+	if(str[i]!='\0')
+	{
+	cout<<str[i];
+	}
+	}
+}
+
+int main(int argc,char *argv[])
+{
+	char *str;
+	str=argv[1];
+	int len=strlen(str);
+	remove_duplicate(str,len);
+	return 0;
+}
