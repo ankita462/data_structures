381c0f42831fbb828d44638e62c26d7a9d1260fe
Ankita Garg <ankitagarg462@gmail.com>
2019-08-02 11:43:07 +0530

    remove duplicate

--- remove_duplicate.cpp
+++ remove_duplicate.cpp
@@ -2,29 +2,22 @@
 #include <cstring>
 using namespace std;
 
-void remove_duplicate(char *str,int len)
-{
-	for(int i=0;i<len;i++)
-	{
-		for(int j=i+1;j<len;j++)
-		{
-			if(str[i]==str[j])
-			{
+void remove_duplicate(char *str,int len) {
+	for(int i=0;i<len;i++) {
+		for(int j=i+1;j<len;j++) {
+			if(str[i]==str[j]) {
 				str[j]='\0';
 			}
 		}
 	}
-	for(int i=0;i<len;i++)
-	{
-	if(str[i]!='\0')
-	{
-	cout<<str[i];
-	}
+	for(int i=0;i<len;i++) {
+		if(str[i]!='\0') {
+			cout<<str[i];
+		}
 	}
 }
 
-int main(int argc,char *argv[])
-{
+int main(int argc,char *argv[]) {
 	char *str;
 	str=argv[1];
 	int len=strlen(str);
